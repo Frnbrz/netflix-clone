cdda7f0ff09c4fbcb5f8feef1816ba893f6a9792
frnbrz <frnbrz@gmail.com>
2025-01-27 20:12:18 +0100

    feat: Add SliderVideo component and implement mobile Navbar with toggle functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import { Navbar } from "@/components/Shared/Navbar/Navbar";
+import SliderVideo from "@/app/(routes)/(home)/components/SliderVideo/SliderVideo";
 
 export default function Home() {
   return (
     <div>
+      <SliderVideo />
       <Navbar />
     </div>
   );
