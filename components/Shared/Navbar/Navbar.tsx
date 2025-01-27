851474ac7ab7590ccbcbc4d17ee55404ba40a834
frnbrz <frnbrz@gmail.com>
2025-01-27 11:06:36 +0100

    refactor: Update Navbar structure and styles for improved layout and responsiveness

--- components/Shared/Navbar/Navbar.tsx
+++ components/Shared/Navbar/Navbar.tsx
@@ -5,13 +5,13 @@ import NavbarMobile from "@/components/Shared/Navbar/NavbarMobile/NavbarMobile";
 export function Navbar() {
 
   return (
-    <nav>
+    <header>
       <div className="container-desktop">
         <NavbarDesktop />
       </div>
       <div className="container-mobile">
         <NavbarMobile />
       </div>
-    </nav>
+    </header>
   );
 }
--- components/Shared/Navbar/NavbarDesktop/NavbarDesktop.tsx
+++ components/Shared/Navbar/NavbarDesktop/NavbarDesktop.tsx
@@ -1,39 +1,33 @@
+"use client"
+
 import { BellRing, Search } from "lucide-react";
 import Link from "next/link";
 import { Logo } from "@/components/Shared/Logo/Logo";
+import { itemsNavbar } from "@/data/itemsNavbar";
+import { useScrollPosition } from "@/hooks/useScrollPosition";
 
 export default function NavbarDesktop() {
+  const scrollPosition = useScrollPosition()
+
+  console.log(scrollPosition)
   return (
-    <nav className="navbar__desktop">
+    <nav className={(scrollPosition ? 'navbar__desktop--bottom' : 'navbar__desktop--top') + " navbar__desktop"}>
       <div className="navbar__desktop__left">
         <Logo />
         <ul className="navbar__desktop__menu">
-          <li className="navbar__desktop__item">
-            <Link href='#' className="navbar__desktop__link">
-              Home
-            </Link>
-          </li>
-          <li className="navbar__desktop__item">
-            <Link href='#' className="navbar__desktop__link">
-              Movies
-            </Link>
-          </li>
-          <li className="navbar__desktop__item">
-            <Link href='#' className="navbar__desktop__link">
-              Series
-            </Link>
-          </li>
-          <li className="navbar__desktop__item">
-            <Link href='#' className="navbar__desktop__link">
-              Profile
-            </Link>
-          </li>
+          {itemsNavbar.map(item => (
+            <li key={item.name} className="navbar__desktop__item">
+              <Link href={item.link} className="link">
+                {item.name}
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
       <div className="navbar__desktop__right">
         <div className="navbar__desktop__icons">
-          <Search className="icon" />
-          <BellRing className="icon" />
+          <Search className="icon" aria-label="Search" />
+          <BellRing className="icon" aria-label="Notifications" />
         </div>
         <div className="navbar__desktop__profile">
           <span>User</span>
