625e91c644771bacb6f5ee80ed5be52196aaf06c
frnbrz <frnbrz@gmail.com>
2025-01-27 11:06:41 +0100

    feat: Add itemsNavbar data structure for navigation links

---
+++ data/itemsNavbar.ts
@@ -0,0 +1,7 @@
+export const itemsNavbar = [
+  { name: 'Home', link: '/' },
+  { name: 'Movies', link: '/movies' },
+  { name: 'Series', link: '/series' },
+  { name: 'More Views', link: '/more-views' },
+  { name: 'My list', link: '/my-list' },
+];
