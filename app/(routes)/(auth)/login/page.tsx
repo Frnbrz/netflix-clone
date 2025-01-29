8024fb2dc28e031099137f6605309be7535d1f49
frnbrz <frnbrz@gmail.com>
2025-01-29 10:09:00 +0100

    feat: Add Terms component and remember me checkbox to login page

--- app/(routes)/(auth)/login/page.tsx
+++ app/(routes)/(auth)/login/page.tsx
@@ -1,9 +1,17 @@
 import Link from "next/link";
+import { Terms } from "@/app/(routes)/(auth)/components/Terms/Terms";
 
 export default function LoginPage() {
   return (
     <div className="login-page">
       <p className="login-page__header">Iniciar sesión</p>
+
+      <div className="login-page__remember">
+        <input type="checkbox" id="terms" className="login-page__remember__input" />
+        <label htmlFor="terms" className="login-page__remember__label">
+          Recuérdame
+        </label>
+      </div>
 
       <div className="login-page__footer">
         <p className="login-page__footer--text">¿Todavía sin Netflix?</p>
@@ -11,6 +19,7 @@ export default function LoginPage() {
           Suscríbete ya
         </Link>
       </div>
+      <Terms />
     </div>
   )
 }
