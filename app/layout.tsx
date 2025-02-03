764f88fcc80bd1108220c59678ad5e21dd5f67cd
frnbrz <frnbrz@gmail.com>
2025-02-03 15:05:21 +0100

    feat: Enhance layout with responsive design adjustments and data attribute for HTML tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en"
+      data-lt-installed="true">
       <body>
         {children}
       </body>
