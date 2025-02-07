3b10fcf507d10681d57ca078bc440deb4e76fa62
frnbrz <frnbrz@gmail.com>
2025-02-07 10:22:18 +0100

    feat: Add ToastContainer for improved user notifications in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Navbar } from "@/components/Shared/Navbar/Navbar";
 import SliderVideo from "@/app/(routes)/(home)/components/SliderVideo/SliderVideo";
 import Footer from "@/components/Shared/Footer/Footer";
+import { ToastContainer } from "react-toastify";
 
 export default function Home() {
   return (
