9e27563129195fcb6978edfd13d45c1ce0065e1e
frnbrz <frnbrz@gmail.com>
2025-02-10 10:57:24 +0100

    feat: Add login action with validation and error handling

---
+++ src/actions/login.ts
@@ -0,0 +1,38 @@
+"use server"
+
+import { LoginFormSchema } from "@/types/auth/login.type"
+import { AuthError } from "next-auth"
+import { signIn } from "@/auth"
+import { z } from "zod"
+
+
+
+export const login = async (values: z.infer<typeof LoginFormSchema>) => {
+  const validatedFields = LoginFormSchema.safeParse(values)
+
+  if (!validatedFields.success) {
+    return { error: "Invalid fields!" }
+  }
+
+  const { email, password } = validatedFields.data
+
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: "/profiles"
+    })
+
+    return { success: true }
+
+  } catch (error) {
+    if (error instanceof AuthError) {
+      if (error.type === "CredentialsSignin") {
+        return { error: "Invalid credentials" }
+      }
+
+      return { error: "Something went wrong!" }
+    }
+
+  }
+}
